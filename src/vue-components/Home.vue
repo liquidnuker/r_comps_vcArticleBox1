c655033971124315a8182d887fdeff6f9ce8e543
administrator <liquidnuker@users.noreply.github.com>
2017-09-26 08:40:34 -0700

    vcArticleBox101 mounted

--- src/vue-components/Home.vue
+++ src/vue-components/Home.vue
@@ -1,16 +1,26 @@
 <template>
-  <div>
-    Home.vue
-  </div>
+  <!-- vcArticleBox101 -->
+  <div class="row">
+    <h2 class="item-id">vcArticleBox101</h2>
+      <div class="col-sm-3">39</div>
+      <div class="col-sm-9">
+      <vcArticleBox101 />
+      </div>
+    </div>
+    <!-- end vcArticleBox101 -->
 </template>
 <script>
+  const vcArticleBox101 = () =>
+    import ('./vcArticleBox101.vue');
   export default {
-    data () {
-      return {    
+    data() {
+        return {}
+      },
+      components: {
+        vcArticleBox101: vcArticleBox101
+      },
+      mounted: function () {
+        // console.log("Home.vue mounted");
       }
-    },
-    mounted: function () {
-      console.log("Home.vue mounted");
-    }
   }
 </script>
---
+++ src/vue-components/vcArticleBox101.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+  <!-- begin vcarticlebox101 -->
+  <div class="vcarticlebox101">
+    <ul>
+      <!-- begin list block 1-->
+      <li class="row">
+        <div class="vcarticlebox101_img-holder">
+          <img src="img.jpg" alt="img alt" title="img title">
+        </div>
+        <article role="article">
+          <h2 class="vcarticlebox101_heading">Article heading</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </article>
+        <footer class="row vcarticlebox101_extras">
+          <p>extra text Lorem ipsum dolor sit amet</p>
+          <button data-message="button1" class="btn btn1-01" tabindex="0">read more</button>
+          <button data-message="button2" class="btn btn1-01" tabindex="0">read more</button>
+        </footer>
+      </li>
+      <!-- end list block 1-->
+    </ul>
+  </div>
+  <!-- end vcarticlebox101 -->
+</div>
+</template>
+<script>
+export default {
+  data () {
+    return {   
+
+    }
+  },
+  watch: {
+  // : function () {
+  // }
+  },
+  props: [
+    "prName1"
+  ],
+  components: {
+  },
+  mounted: function () {
+    console.log("vcvcArticleBox101.vue mounted");
+  },
+  methods: {        
+  }
+}
+</script>
